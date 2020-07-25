fe65603d3883e9e16037882faf9a028a2c013de0
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-07-25 14:47:18 +1200

    Add initial docs for decyclify function

--- decyclify/functions.py
+++ decyclify/functions.py
@@ -17,8 +17,20 @@
 # under the License.
 #
 
+from typing import Iterable
+
+
 def decyclify(number_of_cycles=1):
+    """
+    Decyclify a directed cyclic graph (DCG) into a directed acyclic graph (DAG) with cycles.
+
+    :param number_of_cycles: number of cycles to be generated
+    :type number_of_cycles: int
+    :return: a DCG that is iterable and contains multiple cycles, each cycle with a single DAG
+    :rtype: Iterable
+    """
     if not isinstance(number_of_cycles, int):
         raise TypeError(f"Number of cycles must be an integer, but '{type(number_of_cycles)}' given")
     if number_of_cycles < 1:
         raise ValueError(f"Number of cycles must be at least '1', but '{number_of_cycles}' given")
+    return []
