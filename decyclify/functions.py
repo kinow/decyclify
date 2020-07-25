4b4ba75a7399735c3af3e0b2b18fc38761c2f25d
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-07-25 16:03:25 +1200

    Add graph parameter and more comments

--- decyclify/functions.py
+++ decyclify/functions.py
@@ -17,18 +17,22 @@
 # under the License.
 #
 
-from typing import Iterable
+from typing import Any, Iterable
 
 
-def decyclify(number_of_cycles=1):
+def decyclify(graph: Any, number_of_cycles: int=1):
     """
     Decyclify a directed cyclic graph (DCG) into a directed acyclic graph (DAG) with cycles.
 
+    :param graph: the graph
+    :type graph: Any
     :param number_of_cycles: number of cycles to be generated
     :type number_of_cycles: int
     :return: a DCG that is iterable and contains multiple cycles, each cycle with a single DAG
     :rtype: Iterable
     """
+    if not graph:
+        raise ValueError(f"Graph argument is required, but {graph} given")
     if not isinstance(number_of_cycles, int):
         raise TypeError(f"Number of cycles must be an integer, but '{type(number_of_cycles)}' given")
     if number_of_cycles < 1:
