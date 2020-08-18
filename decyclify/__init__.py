2daa6f7821f3684027b2211f2d859a243097ac0c
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-08-18 18:07:46 +1200

    Import full package name instead of relative

--- decyclify/__init__.py
+++ decyclify/__init__.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from functions import *
+from decyclify.functions import *
 
 __version__ = '0.1.dev0'
 
